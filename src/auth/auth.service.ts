8f355ea2ff46ef03e2611eb07b1dd039986f2894
Rodrigo Liberti <liberti1991@gmail.com>
2024-10-12 17:45:08 -0300

    feat: create jwt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,22 @@
 /* eslint-disable prettier/prettier */
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { compare } from 'bcrypt';
+import { ReturnUserDto } from 'src/user/dtos/returnUser.dto';
 import { UserEntity } from 'src/user/entities/user.entity';
 import { UserService } from 'src/user/user.service';
 import { LoginDto } from './dtos/login.dto';
+import { LoginPayload } from './dtos/loginPayload.dto';
+import { ReturnLogin } from './dtos/returnLogin.dto';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userService: UserService) { }
+  constructor(
+    private readonly userService: UserService,
+    private jwtService: JwtService
+  ) { }
 
-  async login(loginDto: LoginDto): Promise<UserEntity> {
+  async login(loginDto: LoginDto): Promise<ReturnLogin> {
     const user: UserEntity | undefined = await this.userService.findUserByEmail(loginDto.email).catch(() => undefined);
 
     const isMatch = await compare(loginDto.password, user?.password || '');
@@ -18,6 +25,9 @@ export class AuthService {
       throw new NotFoundException('Email ou Senha invalido');
     }
 
-    return user
+    return {
+      accessToken: this.jwtService.sign({ ...new LoginPayload(user) }),
+      user: new ReturnUserDto(user),
+    }
   }
 }
--- src/auth/dtos/login.dto.ts
+++ src/auth/dtos/login.dto.ts
@@ -2,9 +2,9 @@
 import { IsString } from 'class-validator';
 
 export class LoginDto {
-  @IsString()
+  @IsString({ message: 'O campo "email" deve ser uma string válida.' })
   email: string;
 
-  @IsString()
+  @IsString({ message: 'O campo "password" deve ser uma string válida.' })
   password: string;
 }

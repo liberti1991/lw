bb8444cedd8730d1fb8231be717a29945645d0c1
Rodrigo Liberti <liberti1991@gmail.com>
2024-10-12 22:09:38 -0300

    feat: insert unit tests in state

---
+++ src/state/__mocks__/state.mock.ts
@@ -0,0 +1,9 @@
+/* eslint-disable prettier/prettier */
+import { StateEntity } from "../entities/state.entity";
+
+export const stateMock: StateEntity = {
+  createdAt: new Date(),
+  id: 432432,
+  name: 'stateNameMock',
+  updatedAt: new Date(),
+};
---
+++ src/state/__tests__/state.service.spec.ts
@@ -0,0 +1,45 @@
+/* eslint-disable prettier/prettier */
+import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { stateMock } from '../__mocks__/state.mock';
+import { StateEntity } from '../entities/state.entity';
+import { StateService } from '../state.service';
+
+describe('StateService', () => {
+  let service: StateService;
+  let stateRepository: Repository<StateEntity>;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        StateService,
+        {
+          provide: getRepositoryToken(StateEntity),
+          useValue: {
+            find: jest.fn().mockResolvedValue([stateMock]),
+          },
+        },
+      ],
+    }).compile();
+    service = module.get<StateService>(StateService);
+    stateRepository = module.get<Repository<StateEntity>>(
+      getRepositoryToken(StateEntity),
+    );
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+    expect(stateRepository).toBeDefined();
+  });
+
+  it('should return list of states', async () => {
+    const state = await service.getAllState();
+    expect(state).toEqual([stateMock]);
+  });
+
+  it('should return error in exception', async () => {
+    jest.spyOn(stateRepository, 'find').mockRejectedValueOnce(new Error());
+    expect(service.getAllState()).rejects.toThrowError();
+  });
+});
